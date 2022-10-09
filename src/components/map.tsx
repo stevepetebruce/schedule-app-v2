df29415d08b64a05e23eb2f6496dbd8c943a753f
stevepetebruce <stevepetebruce@hotmail.com>
2022-10-09 22:34:44 +0100

    add mapbox

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,10 +1,41 @@
-// import { useRef, useState } from "react";
-// import Link from "next/link";
-// import { Image } from "cloudinary-react";
-// import ReactMapGL, { Marker, Popup, ViewState } from "react-map-gl";
-// import "mapbox-gl/dist/mapbox-gl.css";
+import { FunctionComponent, useRef, useState } from "react";
+import Link from "next/link";
+import { Image } from "cloudinary-react";
+import ReactMapGL, { Marker, Popup, ViewState } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { mapControlDefaultProps } from "react-map-gl/src/components/use-map-control";
 // import { useLocalState } from "src/utils/useLocalState";
 // import { HousesQuery_houses } from "src/generated/HousesQuery";
 // import { SearchBox } from "./searchBox";
 
-export {};
+interface IProps {}
+
+const Map: FunctionComponent<IProps> = ({} ) => {
+
+  const mapRef = useRef<{} | null>(null);
+
+  // state of map long, lat and zoom
+  const [viewport, setViewport] =  useState<ViewState>({
+    latitude: 50.822529,
+    longitude: -0.137163,
+    zoom: 14
+  })
+  return (
+    <div className="text-black relative">
+      <ReactMapGL 
+        {...viewport} 
+        width="100%" 
+        height="calc(100vh - 64px)"
+        mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
+        onViewStateChange={(nextViewport: ViewState) => setViewport(nextViewport)}
+        ref={(instance) => (mapRef.current = instance)}
+        minZoom={5}
+        maxZoom={16}
+        mapStyle="mapbox://styles/leighhalliday/ckhjaksxg0x2v19s1ovps41ef"
+        >
+      </ReactMapGL>
+    </div>
+  )
+}
+
+export default Map

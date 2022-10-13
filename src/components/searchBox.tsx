3931fcdd1f98c75107096699562cd11a7995b80f
stevepetebruce <stevepetebruce@hotmail.com>
2022-10-13 22:16:01 +0100

    Google Places search results from input field

--- src/components/searchBox.tsx
+++ src/components/searchBox.tsx
@@ -1,17 +1,83 @@
-// import { ChangeEvent } from "react";
-// import { FunctionComponent } from "react";
-// import usePlacesAutocomplete, {
-//   getGeocode,
-//   getLatLng,
-// } from "use-places-autocomplete";
-// import { useGoogleMapsScript, Libraries } from "use-google-maps-script";
-// import {
-//   Combobox,
-//   ComboboxInput,
-//   ComboboxPopover,
-//   ComboboxList,
-//   ComboboxOption,
-// } from "@reach/combobox";
-// import "@reach/combobox/styles.css";
+import { ChangeEvent } from "react";
+import { FunctionComponent } from "react";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import { useGoogleMapsScript, Libraries } from "use-google-maps-script";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
 
-export {};
+interface ISeachBoxProps {
+  onSelectAddress: (
+    address: string,
+    longitude: number | null,
+    latitude: number | null,
+  ) => void;
+  defaultValue: string
+}
+
+const libraries: Libraries = ["places"]
+
+const SearchBox = ({onSelectAddress, defaultValue}: ISeachBoxProps) => {
+  const {isLoaded, loadError} = useGoogleMapsScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? "",
+    libraries
+  })
+
+  if(!isLoaded) return null;
+
+  if(loadError) return <div>Error Loading</div>
+
+  return (
+    <ReadySearchBox 
+      onSelectAddress={onSelectAddress} 
+      defaultValue={defaultValue} 
+    />
+  )
+}
+
+const ReadySearchBox = ({onSelectAddress, defaultValue}: ISeachBoxProps) => {
+  const {
+    ready, 
+    value, 
+    setValue, 
+    suggestions: {status, data}, 
+    clearSuggestions
+  } = usePlacesAutocomplete({ debounce: 300, defaultValue})
+
+  const handleSelect = async(address: string) => {
+    console.log({address})
+  }
+
+  console.log({status, data})
+
+  const handleChange = (e :ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+    if(e.target.value === "") {
+      onSelectAddress("", null, null)
+    }
+  }
+
+  return (
+    <Combobox onSelect={handleSelect} >
+      <ComboboxInput 
+        id="search" 
+        value={value} 
+        onChange={handleChange} 
+        disabled={!ready} 
+        placeholder="Search Location"
+        className="w-full p-2"
+        autoComplete="off"
+      />
+    </Combobox>
+  )
+}
+
+export default SearchBox
--- src/components/venueForm.tsx
+++ src/components/venueForm.tsx
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form";
 // import { useRouter } from "next/router";
 // import Link from "next/link";
 // import { Image } from "cloudinary-react";
-// import { SearchBox } from "./searchBox";
+import SearchBox from "./searchBox";
 // import {
 //   CreateHouseMutation,
 //   CreateHouseMutationVariables,
@@ -49,7 +49,13 @@ function venueForm({}: IProps) {
       <h1 className="text-xl">Add Venue</h1>
       <div className="mt-4">
         <label htmlFor="search" className="block">Search for venue address</label>
-        {/* Add Search field */}
+        <SearchBox onSelectAddress={(address, latitude, longitude) => {
+            setValue("address", address)
+            setValue("latitude", latitude)
+            setValue("longitude", longitude)
+          }}
+          defaultValue="" 
+        />
         {errors.address && <p>{errors.address.message}</p>}
       </div>
     </form>

8be99df3992bf6cfd5dcd5e4b80b78f784cbf78f
stevepetebruce <stevepetebruce@hotmail.com>
2022-10-19 23:14:19 +0100

    Add venue forms

--- src/components/venueForm.tsx
+++ src/components/venueForm.tsx
@@ -1,8 +1,8 @@
+import Link from "next/link";
 import { useState, useEffect, ChangeEvent } from "react";
 import { useForm } from "react-hook-form";
 // import { useMutation, gql } from "@apollo/client";
 // import { useRouter } from "next/router";
-// import Link from "next/link";
 // import { Image } from "cloudinary-react";
 import SearchBox from "./searchBox";
 // import {
@@ -20,6 +20,7 @@ interface IFormData {
   address: string,
   latitude: number,
   longitude: number,
+  capacity: number,
   image: FileList
 }
 
@@ -27,23 +28,28 @@ interface IProps {}
 
 function venueForm({}: IProps) {
   const [submitting, setSubmitting] = useState(false)
-
+  const [previewImage, setPreviewImage] = useState<string>();
   const {register, handleSubmit, setValue, errors, watch } = useForm<IFormData>({defaultValues: {}})
 
   const address = watch("address")
 
   useEffect(() => {
     register({ name: "venue" }, { required: "Please enter the venue" })
+    register({ name: "capacity" }, { required: true, min: 1 })
     register({ name: "address" }, { required: "Please enter the address" })
     register({ name: "latitude" }, { required: true, min: -90, max: 90 })
     register({ name: "longitude" }, { required: true, min: -180, max: 180 })
 
   }, [register])
+
+  const handleCreate = (data: {}) => {
+    console.log(data)
+  }
   
   const onSubmit = (data: IFormData) => {
     setSubmitting(true)
-    //handleCreate(data)
-    //setSubmitting(false)
+    handleCreate(data)
+    setSubmitting(false)
   }
 
   return (
@@ -61,6 +67,61 @@ function venueForm({}: IProps) {
         {errors.address && <p>{errors.address.message}</p>}
         <h2>{address}</h2>
       </div>
+      {address && 
+        <>
+          <div className="mt-4">
+            <label htmlFor="image" className="p-4 border-dashed border-4 border-gray-600 block cursor-pointer">Click to add venue image</label>
+            <input 
+              id="image" 
+              name="image" 
+              type="file" 
+              accept="image/*" 
+              style={{ display: "none" }} 
+              ref={register({
+                validate: (fileList: FileList) => {
+                  if(fileList.length === 1) return true;
+                  return "Please upload an image"
+                }
+              })}
+              onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                if (event?.target?.files?.[0]) {
+                  const file = event.target.files[0];
+                  const reader = new FileReader();
+                  reader.onloadend = () => {
+                    setPreviewImage(reader.result as string)
+                    console.log(reader.result)
+                  };
+                  reader.readAsDataURL(file)
+                }
+              }} 
+            />
+            {previewImage && <img src={previewImage} className="mt-4 object-cover" style={{width: "576px", height: `${(9/16)*576}px`}} />}
+            {errors.image && <p>{errors.image.message}</p> }
+          </div>
+
+          <div className="mt-4">
+            <label htmlFor="venue" className="block">Venue</label>
+            <input type="text" name="venue" id="venue" className="p-2" ref={register({
+              required: "Please enter the venue name"
+            })} />
+            {errors.venue && errors.venue.message}
+          </div>
+
+          <div className="mt-4">
+            <label htmlFor="capacity" className="block">Capacity</label>
+            <input type="number" min={1} name="capacity" id="capacity" className="p-2" ref={register({
+              required: "Please enter the venue capacity"
+            })} />
+            {errors.capacity && errors.capacity.message}
+          </div>
+          <div className="mt-4">
+            <button className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded" type="submit" disabled={submitting}>Save</button>
+            <Link href="/">
+              <a className="ml-4">Cancel</a>
+            </Link>
+          </div>
+        </>
+      }
     </form>
   )
 }

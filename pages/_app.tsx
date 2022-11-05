f429db2fa4fc250a6d71736e09af96f377bbed78
stevepetebruce <stevepetebruce@hotmail.com>
2022-11-05 22:47:33 +0000

    Apollo Client Setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,24 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
-// import { ApolloProvider } from "@apollo/client";
-// import { useApollo } from "src/apollo";
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "src/apollo";
 import { AuthProvider } from "src/auth/useAuth";
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+
+  const apolloClient = useApollo();
+
   return (
 
     <AuthProvider>
-      <Head>
-        <title>Schedule Live</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Component {...pageProps} />
-
+      <ApolloProvider client={apolloClient}>
+        <Head>
+          <title>Schedule Live</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </AuthProvider>
   );
 }
--- src/apollo.ts
+++ src/apollo.ts
@@ -1,5 +1,26 @@
-// import { ApolloClient, InMemoryCache } from "@apollo/client";
-// import { HttpLink } from "@apollo/client/link/http";
-// import { useMemo } from "react";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { HttpLink } from "@apollo/client/link/http";
+import { useMemo } from "react";
 
-export {};
+// Apollo Client Setup
+
+function createApolloClient() {
+  return new ApolloClient({
+    ssrMode: typeof window === "undefined",
+    link: new HttpLink({
+      uri: "/api/graphql",
+      credentials: "same-origin",
+    }),
+    defaultOptions: {
+      watchQuery: {
+        fetchPolicy: "cache-and-network",
+      },
+    },
+    cache: new InMemoryCache(),
+  });
+}
+
+export function useApollo() {
+  const client = useMemo(() => createApolloClient(), []);
+  return client;
+}
